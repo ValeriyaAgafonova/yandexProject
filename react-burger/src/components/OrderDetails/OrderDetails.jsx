c46e59add78f3521e95b516fc4650500551cf2e6
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2022-05-27 21:20:53 +0300

    refactor: add gif to modal with order and change background-color

--- react-burger/src/components/OrderDetails/OrderDetails.jsx
+++ react-burger/src/components/OrderDetails/OrderDetails.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Styles from "./OrderDetails.module.css";
-import { CheckMarkIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 
 const OrderDetails = () => {
   return (
     <div className={Styles.details}>
       <p className="text text_type_digits-large">122432</p>
       <p className="text text_type_main-default">идентификатор заказа</p>
-      <div className={Styles.round}>
-        <CheckMarkIcon type="primary" />
-      </div>
+      <img
+        className={Styles.done}
+        src={require("../../images/done.gif")}
+        alt="done"
+      />
       <p className="text text_type_main-default">ваш заказ начали готовить</p>
       <p className="text text_type_main-default text_color_inactive">
         Дождитесь готовности на орбитальной станции
